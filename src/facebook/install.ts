7b40d4d45d762d69ccc72511a0907326a70a3a5b
Cesar Varela <mail@cesarvarela.com>
2017-02-19 00:09:44 -0300

    add emoji normalization for facebook

--- src/facebook/install.ts
+++ src/facebook/install.ts
@@ -1,4 +1,5 @@
-import { ThreadSettings } from './ThreadSettings';
+import { UniversalBot } from 'botbuilder';
+import { ThreadSettings } from './model/ThreadSettings';
 
 
 export interface IFacebookModuleSettings {
@@ -8,14 +9,34 @@ export interface IFacebookModuleSettings {
     greetingText?: string;
 }
 
-export function install(config: IFacebookModuleSettings) {
+export function install(bot: UniversalBot, config: IFacebookModuleSettings) {
 
     let threadSettings = new ThreadSettings(config.FACEBOOK_PAGE_TOKEN);
 
-    if(config.greetingText)
-    {
+    if (config.greetingText) {
         threadSettings.greeting(config.greetingText);
     }
 
+    bot.use(
+        {
+            receive: (event, next) => {
+
+                if (event.source == 'facebook') {
+
+                    switch (event.sourceEvent.message.sticker_id) {
+                        case 369239263222822:
+
+                            event.text = ':like:'
+                            break;
+                    }
+                }
+
+                next()
+            }
+        })
+
     //should do the same with all the other thread settings
+
+
+
 }
