d48c3d5766c7fcfe60960e035434c96c3b39ae2b
Clara Youdale <clarayoudale@outlook.com>
2017-02-28 15:02:41 -0300

    jump lines

--- src/zendesk/model/Api.ts
+++ src/zendesk/model/Api.ts
@@ -24,35 +24,50 @@ export class Api {
 
     getTicket(id: number): Promise<any> {
         var self = this
+
         let tickets = this.zendesk.tickets.show(id).then(function(result){
+
             return self.asMessage(result)
+        
         });
+        
         return Promise.resolve(tickets);
     }
 
     createTicket(ticket: any):Promise<any>{
+       
        let retticket = this.zendesk.tickets.create({
+            
             subject: ticket.subject,
+            
             comment: {
                 body: ticket.comment
             },
+            
             custom_fields: ticket.custom_fields
+        
         }).then(function(result){
             
             return result.ticket;
 
         });
+        
         return Promise.resolve(retticket);
     }
 
     asMessage(ticket: any):string {
+        
         let msg = ""
+        
         msg = msg.concat("Nro: ").concat(ticket.id.toString())
                     .concat("Estado: ").concat(ticket.status)
                     .concat("Problema: " + ticket.subject)
+        
         for (var i =0; i < ticket.custom_fields.length; i++) {
+            
             msg = msg.concat(ticket.custom_fields[i].value)
         }
+        
         return msg        
     }
 }
