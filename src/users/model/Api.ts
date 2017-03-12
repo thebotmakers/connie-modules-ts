b024a10ec2184237cb223d1418d9f264c5a9eda3
Cesar Varela <mail@cesarvarela.com>
2017-03-11 22:55:40 -0300

    add pagination to users module

--- src/users/model/Api.ts
+++ src/users/model/Api.ts
@@ -4,6 +4,10 @@ import { User } from './User';
 import { Server } from 'restify'
 import { Db, Collection, UpdateWriteOpResult } from 'mongodb'
 
+export interface IPaginatedResponse<T> {
+    total: number;
+    items: T[];
+}
 
 export class Api {
 
@@ -14,7 +18,26 @@ export class Api {
         this.collection = db.collection('users')
     }
 
-    get(connieId: string): Promise<User> {
+    get(page: number = 1, pageSize: number = 10): Promise<IPaginatedResponse<User>> {
+
+        return this.collection.count({})
+            .then(result => {
+
+                return this.collection
+                    .find({})
+                    .skip((page - 1) * pageSize)
+                    .limit(pageSize)
+                    .toArray()
+                    .then(users => {
+                        return {
+                            total: result,
+                            items: users
+                        }
+                    })
+            })
+    }
+
+    getById(connieId: string): Promise<User> {
 
         return this.collection.find({ connieId: connieId }).limit(1).next()
     }
